47d717d934dd11f5cc913868b83e472fe7f10202
Yesenia Medina <yesekcl@gmail.com>
2021-04-19 15:44:53 -0500

    Example Django

---
+++ PruebaDJ/PruebaDJ/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for PruebaDJ project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PruebaDJ.settings')
+
+application = get_asgi_application()
---
+++ PruebaDJ/PruebaH/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PruebahConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'PruebaH'
